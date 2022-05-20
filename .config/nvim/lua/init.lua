d0649f3dbca2a7fa30cf1f3ce8cd8cb1577800ed
Santeri Kääriäinen <santeri.kaariainen@nitor.com>
2022-05-20 14:45:04 +0300

    add init.vim

---
+++ .config/nvim/lua/init.lua
@@ -0,0 +1,23 @@
+vim.g.mapleader = " "
+vim.opt.rnu = true
+require'plugins'
+
+vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>fs', '<cmd>Telescope grep_string<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', { noremap = true })
+require'nvim-tree'.setup {
+  update_focused_file = {
+    enable = true,
+    update_cwd = true,
+    ignore_list = {},
+  },
+}
+
+
+vim.api.nvim_set_keymap('n', '<C-n>', '<cmd>NvimTreeToggle<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>r', '<cmd>NvimTreeRefresh<cr>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>n', '<cmd>NvimTreeFindFile<cr>', { noremap = true })
+
+vim.api.nvim_set_keymap('n', '<leader>w', '<C-w>', { noremap = true })
---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,34 @@
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
+
+return require('packer').startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use 'tpope/vim-fugitive'
+
+  use 'tpope/vim-surround'
+
+  use 'tpope/vim-commentary'
+
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use 'nvim-treesitter/nvim-treesitter'
+
+
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = {
+      'kyazdani42/nvim-web-devicons', -- optional, for file icon
+    },
+    tag = 'nightly' -- optional, updated every week. (see issue #1193)
+  }
+
+    use 'neovim/nvim-lspconfig'
+end)
