3043dfb93e29503ed3a8c492da4a59bd08f67e2b
Napuu <santeri.kaariainen@iki.fi>
2023-05-07 11:25:25 +0300

    basic configs

--- .config/nvim/lua/common.lua
+++ .config/nvim/lua/common.lua
@@ -7,3 +7,8 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = ","
 vim.g.localleader = ","
 
+-- how often stuff is checked (e.g. git next to line would be slow otherwise)
+vim.api.nvim_command('set updatetime=100')
+--?? not sure about these ones
+vim.api.nvim_command('set laststatus=3')
+vim.api.nvim_command('set completeopt=menu,menuone,noselect')
